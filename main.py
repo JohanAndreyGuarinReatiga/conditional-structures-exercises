76f1084a7a1ccdaca232e1f25e088e4e73d80667
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2024-10-31 15:00:11 -0500

    letra o numero

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-#División¶
-#Escriba un programa que pida dos números enteros y que calcule la división, indicando si la división es exacta o no.
+#Letra o número
+#Escriba un programa que determine si un caracter ingresado es letra, número, o ninguno de los dos. En caso que sea letra, determine si es mayúscula o minúscula.
 
-FirstNumber = int(input("Enter the numerator: "))
-SecondNumber = int(input("Enter the divisor: "))
 
-if SecondNumber != 0: 
-    Result = int(FirstNumber / SecondNumber)
-    if FirstNumber % SecondNumber == 0:
-        print(f"""The result is {Result}  and the division is exact""")
-    else: 
-        print(f"Thats not an exact division and its result is {Result} ")
-else: 
-    print("You cant divide by zero") 
+caracter = input("Write a character, a number, a letter, or either of them: ")
+if caracter.isalpha():
+    if caracter.isupper():
+        print("The character is a capital letter")
+    else:
+        print("The character is a lowercase letter")
+elif caracter.isdigit():
+     print("The caracter is a number")
+else:
+    print("Isnt either of the options")    
+    
+#Ingrese caracter: 9
+#Es numero.
+#Ingrese caracter: A
+#Es letra mayúscula.
+#Ingrese caracter: f
+#Es letra minúscula.
+#Ingrese caracter: #
+#No es letra ni número.
