9b3b6905fd2e7b9d3204b2ad96b92452b9ad56bd
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2024-10-31 17:18:39 -0500

    ordenamiento

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 #Ordenamiento
 #Escriba un programa que reciba como entrada dos números, y los muestre ordenados de menor a mayor:
 
-print("Selecciona una opción:")
-print("1. Ordenar dos números")
-print("2. Ordenar tres números")
-print("3. Ordenar cuatro números")
+print("Choose an option ")
+print("1. Organize two numbers")
+print("2. Organize three numbers")
+print("3. Organize four numbrs")
 
 option = int(input("Enter the number of the desired option: "))
 
 if option == 1:
     num1 = int(input("Enter the first number: "))
     num2 = int(input("Enter de second number: "))
-    sorted_nums = sorted([num1, num2])
-    print("Números ordenados:", " ".join(map(str, sorted_nums)))
+    sortedNums = sorted([num1, num2])
+    print("Numbers in order", " ".join(map(str, sortedNums)))
 elif option == 2:
-    num1 = int(input("Ingrese el primer número: "))
-    num2 = int(input("Ingrese el segundo número: "))
-    num3 = int(input("Ingrese el tercer número: "))
-    sorted_nums = sorted([num1, num2, num3])
-    print("Números ordenados:", " ".join(map(str, sorted_nums)))
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter de second number: "))
+    num3 = int(input("Enter the third number: "))
+    sortedNums = sorted([num1, num2, num3])
+    print("Numbers in order", " ".join(map(str, sortedNums)))
 elif option == 3:
-    num1 = int(input("Ingrese el primer número: "))
-    num2 = int(input("Ingrese el segundo número: "))
-    num3 = int(input("Ingrese el tercer número: "))
-    num4 = int(input("Ingrese el cuarto número: "))
-    sorted_nums = sorted([num1, num2, num3, num4])
-    print("Números ordenados:", " ".join(map(str, sorted_nums)))
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter de second number: "))
+    num3 = int(input("Enter the third number: "))
+    num4 = int(input("Enter the fourth number: "))
+    sortedNums = sorted([num1, num2, num3, num4])
+    print("Numbers in order:", " ".join(map(str, sortedNums)))
 else:
-    print("Opción no válida. Por favor, elige 1, 2 o 3.")
+    print("No valid option. Choose between 1, 2 or 3.")
--- ordenamiento.py
+++
@@ -1,9 +0,0 @@
-
-
-print("Choose which program you will use: ")
-
-#primer programa
-#firstNumber = int(input("Type a number: "))
-#secondNumber = int(input("Type the second number: "))
-#if firstNumber < secondNumber: 
-#    print(F""" {firstNumber} is lower""")
