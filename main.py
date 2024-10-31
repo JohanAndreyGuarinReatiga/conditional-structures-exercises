f06f9dbafee3a83a2d327f5f28a49b9be3797e43
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2024-10-31 14:52:21 -0500

    division

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
-#Años bisiestos
-#Cuando la Tierra completa una órbita alrededor del Sol, no han transcurrido exactamente 365 rotaciones sobre sí misma, sino un poco más. Más precisamente, la diferencia es de más o menos un cuarto de día.
+#División¶
+#Escriba un programa que pida dos números enteros y que calcule la división, indicando si la división es exacta o no.
 
-#Para evitar que las estaciones se desfasen con el calendario, el calendario juliano introdujo la regla de introducir un día adicional en los años divisibles por 4 (llamados bisiestos), para tomar en consideración los cuatro cuartos de día acumulados.
+FirstNumber = int(input("Enter the numerator: "))
+SecondNumber = int(input("Enter the divisor: "))
 
-#Sin embargo, bajo esta regla sigue habiendo un desfase, que es de aproximadamente 3/400 de día.
-
-#Para corregir este desfase, en el año 1582 el papa Gregorio XIII introdujo un nuevo calendario, en el que el último año de cada siglo dejaba de ser bisiesto, a no ser que fuera divisible por 400.
-
-#Escriba un programa que indique si un año es bisiesto o no, teniendo en cuenta cuál era el calendario vigente en ese año:
-
-AB = int(input("Enter a year: "))
-
-if AB < 1582:
-    if AB % 4 == 0:
-        print("Es un anno bisiesto")
-    else:
-        print("No es bisiesto")
-else:
-    if (AB % 400 ==0) or (AB % 4 ==0 and AB % 100 != 0):
-        print("Es un anno bisiesto")
+if SecondNumber != 0: 
+    Result = int(FirstNumber / SecondNumber)
+    if FirstNumber % SecondNumber == 0:
+        print(f"""The result is {Result}  and the division is exact""")
     else: 
-        print("No es bisiesto")
+        print(f"Thats not an exact division and its result is {Result} ")
+else: 
+    print("You cant divide by zero") 
