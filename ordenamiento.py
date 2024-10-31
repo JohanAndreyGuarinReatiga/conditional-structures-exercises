eb38d49c5bbf02bf88e680768003b317a44516fb
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2024-10-31 14:44:48 -0500

    determinar par

--- ordenamiento.py
+++ ordenamiento.py
@@ -2,18 +2,8 @@
 
 print("Choose which program you will use: ")
 
-
-
-
-
-
-
-
-
 #primer programa
 #firstNumber = int(input("Type a number: "))
 #secondNumber = int(input("Type the second number: "))
 #if firstNumber < secondNumber: 
 #    print(F""" {firstNumber} is lower""")
-
-#sin terminar
