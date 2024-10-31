8ffb790475bdc4983abdd567a93207d9683fe552
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2024-10-31 14:49:43 -0500

    annos bisiestos

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-#Determinar par
-#Escriba un programa que determine si el número entero ingresado por el usuario es par o no, en ingles even and odd.
+#Años bisiestos
+#Cuando la Tierra completa una órbita alrededor del Sol, no han transcurrido exactamente 365 rotaciones sobre sí misma, sino un poco más. Más precisamente, la diferencia es de más o menos un cuarto de día.
 
-#Ingrese un número: 4
-#Su número es par
+#Para evitar que las estaciones se desfasen con el calendario, el calendario juliano introdujo la regla de introducir un día adicional en los años divisibles por 4 (llamados bisiestos), para tomar en consideración los cuatro cuartos de día acumulados.
 
-#Ingrese un número: 3
-#Su número es impar
+#Sin embargo, bajo esta regla sigue habiendo un desfase, que es de aproximadamente 3/400 de día.
 
-number = int(input("Enter a number: "))
+#Para corregir este desfase, en el año 1582 el papa Gregorio XIII introdujo un nuevo calendario, en el que el último año de cada siglo dejaba de ser bisiesto, a no ser que fuera divisible por 400.
 
-if number % 2 == 0:
-    print("The number is even")
+#Escriba un programa que indique si un año es bisiesto o no, teniendo en cuenta cuál era el calendario vigente en ese año:
+
+AB = int(input("Enter a year: "))
+
+if AB < 1582:
+    if AB % 4 == 0:
+        print("Es un anno bisiesto")
+    else:
+        print("No es bisiesto")
 else:
-    print("The number is odd")
-    
+    if (AB % 400 ==0) or (AB % 4 ==0 and AB % 100 != 0):
+        print("Es un anno bisiesto")
+    else: 
+        print("No es bisiesto")
